de912b7be965dde3b787ba108d5b2dc2cecf72d9
Nik Rykov <nik@rareweave.store>
2023-09-02 15:38:32 +0300

    some updates

---
+++ SPEC.md
@@ -0,0 +1,7 @@
+# dforms-contracts v1.0.0
+
+Warp Contracts for dForms
+
+`getQuestions` 
+
+> Get questions to trivia
---
+++ src/functions/getQuestions.js
@@ -0,0 +1,6 @@
+module.exports = async function (state, action, handler) {
+    return {result:state.questions}
+}
+/*function-description
+Get questions to trivia
+function-description*/
