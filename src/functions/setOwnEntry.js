001e8f7e0e3ac53c7523b9c076f4a9df7390587e
Nik Rykov <nik@rareweave.store>
2023-09-02 19:11:44 +0300

    spec>readme

--- src/functions/setOwnEntry.js
+++ src/functions/setOwnEntry.js
@@ -1,4 +1,8 @@
+
 module.exports = async function (state, action, handler) {
+    if(state.metadata.maxEntries!=0&&state.entries.length>=state.metadata.maxEntries){
+        throw new ContractError("Sorry, trivia already received max amount of entries.")
+    }
     state.entries.push({
         author:action.caller,
         questions:action.input.questions
